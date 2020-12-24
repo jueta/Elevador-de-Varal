a994cf5d0fbfd7c877faff7a49431bda0ce4eaa3
Joao Pedro Miranda Marques <jpmm.brasil@hotmail.com>
2020-12-24 13:24:13 -0300

    Formatacao do codigo

--- elevador_cpe_1.1_Nome/elavador_cpe_1.1_Nome.ino
+++ elevador_cpe_1.1_Nome/elavador_cpe_1.1_Nome.ino
@@ -1,3 +1,6 @@
+
+// ELEVADOR DE VARAL
+
 #define NOME ""
 
 #include <Wire.h> 
@@ -72,212 +75,246 @@ void calcula() {//calcula a corrente para comparaçao do peso
        verificadescida = 3;  
     }
 }
+
+void salva_subida(){
+    lcd.setBacklight(HIGH);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Salvando Subida");
+    analogWrite(LPWM_Output, 190);
+    analogWrite(RPWM_Output, 0);
+    motor = 1;
+    posicaoAtual1 = 0;
+    posicaoAtual2 = 0;  
+
+    while(motor){       
+        calcula();     
+        if(digitalRead(sensor)==HIGH){//faz a contagem de quantas voltas foram feitas na polia
+            while (digitalRead(sensor) == HIGH); 
+            if(posicaoAtual1 == 255){
+                posicaoAtual1 = 0;
+                posicaoAtual2++;
+            }
+            else{
+                posicaoAtual1++;
+            }
+            lcd.setCursor(0, 1); 
+        }
+        if(digitalRead(SALVASOBE) == HIGH ){//identifica o aperto do botao, para o salvamento da subida
+            while (digitalRead(SALVASOBE) == HIGH);
+            lcd.clear();
+            lcd.print("Subida Salva"); 
+            analogWrite(LPWM_Output, 0); 
+            analogWrite(RPWM_Output, 0); 
+            motor = 0;
+            verificadescida = 3; 
+        }                          
+    }
+        posicaoFinal1 = posicaoAtual1;
+        posicaoFinal2 = posicaoAtual2; 
+        EEPROM.write(0, posicaoFinal1);
+        EEPROM.write(5,posicaoFinal2);
+        EEPROM.write(3, posicaoAtual2);
+        EEPROM.write(2, posicaoAtual1);
+        EEPROM.write(1, verificadescida); 
+}
     
+void salva_descida(){
+    lcd.setBacklight(HIGH); 
+    lcd.clear(); 
+    lcd.setCursor(0, 0);
+    lcd.print("Salvando Descida"); 
+    analogWrite(LPWM_Output, 0); 
+    analogWrite(RPWM_Output, 170); 
+    motor = 1; 
+
+    while(motor){ 
+        if(digitalRead(SALVADESCE) == HIGH ){
+            while (digitalRead(SALVADESCE) == HIGH);
+            lcd.clear();
+            lcd.print("Descida Salva");    
+            analogWrite(LPWM_Output, 0); 
+            analogWrite(RPWM_Output, 0); 
+            motor = 0;
+            verificadescida = 1;
+        }
+    }
+    EEPROM.write(1, verificadescida);
+}
+
+void func_descida(){
+    lcd.setBacklight(HIGH); 
+    lcd.clear(); 
+    lcd.setCursor(0, 0); 
+    lcd.print("Varal Descendo");
+
+    lcd.setCursor(0, 1); 
+    lcd.print(NOME);
+    analogWrite(LPWM_Output, 0); 
+    analogWrite(RPWM_Output, 170); 
+    posicaoFinal1 = EEPROM.read(0);
+    posicaoFinal2 = EEPROM.read(5);
+    lcd.setCursor(0, 0); 
+    lcd.print("Varal Descendo");
+
+    lcd.setCursor(0, 1); 
+    lcd.print(NOME);
+    int posTotal = 255*posicaoFinal2 + posicaoFinal1; 
+    motor = 1;   
+
+    while(motor){     
+        calcula(); 
+        if(digitalRead(sensor)==HIGH){ 
+            while (digitalRead(sensor) == HIGH);
+            if(posTotal > 0){
+                posTotal--;  
+            }
+        }
+        if(posTotal == 0){
+            lcd.clear(); 
+            lcd.print("Chegou embaixo"); 
+            analogWrite(LPWM_Output, 0); 
+            analogWrite(RPWM_Output, 0); 
+            motor = 0;
+        }
+    }
+
+    verificadescida=4;
+    posicaoAtual1 = 0;
+    posicaoAtual2 = 0;
+    EEPROM.write(2, posicaoAtual1);
+    EEPROM.write(3, posicaoAtual2);
+    EEPROM.write(1, verificadescida);
+}
+
+void func_subida(){
+    analogWrite(LPWM_Output, 190); 
+    analogWrite(RPWM_Output, 0); 
+    posicaoFinal1 = EEPROM.read(0);
+    posicaoFinal2 = EEPROM.read(5);
+
+    int posFinalTotal = 0;
+    lcd.setBacklight(HIGH); 
+    lcd.clear(); 
+    lcd.setCursor(0, 0); 
+    lcd.print("Varal Subindo");
+    lcd.setCursor(0, 1); 
+    lcd.print(NOME); 
+    motor = 1;      
+
+    while(motor){
+        calcula();
+        //lcd.setCursor(10, 1);
+        //lcd.print(current);              
+        if(digitalRead(sensor)==HIGH){ 
+            while (digitalRead(sensor) == HIGH);
+            posFinalTotal++;
+            //lcd.setCursor(0, 1); 
+            //lcd.print(posFinalTotal);
+        }            
+        if(posFinalTotal == posicaoFinal1 + posicaoFinal2*255){
+            lcd.clear(); 
+            lcd.print("Chegou no topo"); 
+            analogWrite(LPWM_Output, 0); 
+            analogWrite(RPWM_Output, 0); 
+            motor = 0;}
+            verificadescida=3;
+        }
+
+    posicaoAtual1 = posicaoFinal1;
+    posicaoAtual2 = posicaoFinal2;
+    EEPROM.write(2, posicaoAtual1);
+    EEPROM.write(3, posicaoAtual2);
+    EEPROM.write(1, verificadescida);
+}
+
+
+
+
+
 
 void loop() {    
 
-     if (digitalRead(SALVASOBE) == HIGH && estado==1 && verificadescida==1){ //salva subida
-       while (digitalRead(SALVASOBE) == HIGH);
-       lcd.setBacklight(HIGH);
-       lcd.clear();
-       lcd.setCursor(0, 0);
-       lcd.print("Salvando Subida");
-       analogWrite(LPWM_Output, 190);
-       analogWrite(RPWM_Output, 0);
-       motor = 1;
-       posicaoAtual1 = 0;
-       posicaoAtual2 = 0;  
-       while(motor){       
-             calcula();     
-             if(digitalRead(sensor)==HIGH){//faz a contagem de quantas voltas foram feitas na polia
-              while (digitalRead(sensor) == HIGH); 
-              if(posicaoAtual1 == 255){
-                posicaoAtual1 = 0;
-                posicaoAtual2++;
-              }
-              else{
-                posicaoAtual1++;
-              }
+// ----- SALVAMENTO ------ 
 
-              lcd.setCursor(0, 1); 
-              
-              
-            }
-            if(digitalRead(SALVASOBE) == HIGH ){//identifica o aperto do botao, para o salvamento da subida
-              while (digitalRead(SALVASOBE) == HIGH);
-              lcd.clear();
-              lcd.print("Subida Salva"); 
-              analogWrite(LPWM_Output, 0); 
-              analogWrite(RPWM_Output, 0); 
-              motor = 0;
-              verificadescida = 3; 
-           }                          
-         }        
-         posicaoFinal1 = posicaoAtual1;
-         posicaoFinal2 = posicaoAtual2; 
-         EEPROM.write(0, posicaoFinal1);
-         EEPROM.write(5,posicaoFinal2);
-         EEPROM.write(3, posicaoAtual2);
-         EEPROM.write(2, posicaoAtual1);
-         EEPROM.write(1, verificadescida); 
-     }
+//salva subida
+    if (digitalRead(SALVASOBE) == HIGH && estado==1 && verificadescida==1){
+        while (digitalRead(SALVASOBE) == HIGH);
+        salva_subida();
+    }
 
-
-     else if (digitalRead(SALVADESCE) == HIGH && estado==1 && verificadescida==0){ //salva descida           
-          while (digitalRead(SALVADESCE) == HIGH);
-          lcd.setBacklight(HIGH); 
-          lcd.clear(); 
-          lcd.setCursor(0, 0);
-          lcd.print("Salvando Descida"); 
-          analogWrite(LPWM_Output, 0); 
-          analogWrite(RPWM_Output, 170); 
-          motor = 1; 
-          while(motor){ 
-            if(digitalRead(SALVADESCE) == HIGH ){
-              while (digitalRead(SALVADESCE) == HIGH);
-              lcd.clear();
-              lcd.print("Descida Salva");    
-              analogWrite(LPWM_Output, 0); 
-              analogWrite(RPWM_Output, 0); 
-             motor = 0;
-             verificadescida = 1;
-            }
-          }
-           EEPROM.write(1, verificadescida);
-      }
+//salva descida
+    else if (digitalRead(SALVADESCE) == HIGH && estado==1 && verificadescida==0){         
+        while (digitalRead(SALVADESCE) == HIGH);
+        salva_descida();
+    }
     
-   
 // ----- liga, desliga e reset ------ 
  
-    else if ( estado == 0 && digitalRead(LIGA) == HIGH ) { //liga
+//LIGA
+    else if ( estado == 0 && digitalRead(LIGA) == HIGH ) {
         while (digitalRead(LIGA) == HIGH); 
-         EEPROM.write(1, 0);
-         analogWrite(LPWM_Output, 0); 
-         analogWrite(RPWM_Output, 0); 
-         lcd.setBacklight(HIGH); 
-         lcd.clear(); 
-         lcd.setCursor(0, 0); 
-         lcd.print("Ligado"); 
-         lcd.setCursor(0, 1); 
-         lcd.print("Elevadores Harah"); 
-         estado = 1; 
+        
+        EEPROM.write(1, 0);
+        analogWrite(LPWM_Output, 0); 
+        analogWrite(RPWM_Output, 0); 
+        lcd.setBacklight(HIGH); 
+        lcd.clear(); 
+        lcd.setCursor(0, 0); 
+        lcd.print("Ligado"); 
+        lcd.setCursor(0, 1); 
+        lcd.print("Elevadores Harah"); 
+        estado = 1; 
     }
-     else if ( estado == 1 && digitalRead(LIGA) == HIGH ) { //Desliga
+
+//DESLIGA
+    else if ( estado == 1 && digitalRead(LIGA) == HIGH ) {
         while (digitalRead(LIGA) == HIGH); 
-         analogWrite(LPWM_Output, 0); 
-         analogWrite(RPWM_Output, 0); 
-         lcd.setBacklight(LOW); 
-         lcd.clear(); 
-         estado = 0;
-     }
-     else if(estado == 1 && digitalRead(RESET) == HIGH){ //reset
-      while(digitalRead(RESET) == HIGH);
+
+        analogWrite(LPWM_Output, 0); 
+        analogWrite(RPWM_Output, 0); 
+        lcd.setBacklight(LOW); 
+        lcd.clear(); 
+        estado = 0;
+    }
+
+//RESET
+    else if(estado == 1 && digitalRead(RESET) == HIGH){
+        while(digitalRead(RESET) == HIGH);
+
         lcd.setBacklight(HIGH); 
         lcd.clear(); 
         lcd.setCursor(0, 0); 
         lcd.print("RESET");
         analogWrite(LPWM_Output, 0);
         analogWrite(RPWM_Output, 0);
+
         posicaoFinal1 = 0;
         posicaoFinal2 = 0;
         posicaoAtual1 = 0;
         posicaoAtual2 = 0;
         verificadescida = 0;
         salvamento = 1;
+
         EEPROM.write(0, posicaoFinal1);
         EEPROM.write(2, posicaoAtual1);
         EEPROM.write(3, posicaoAtual2);
         EEPROM.write(5, posicaoFinal2);
         EEPROM.write(1, verificadescida);
-      }        
+    }        
 
-// subida     
+// ----- sobe e desce ------ 
+
+//Subida     
     if ((digitalRead(SOBE) == HIGH && estado==1 && verificadescida==4) || ((digitalRead(SOBE) == HIGH && estado==1 &&  EEPROM.read(2) == 0 && EEPROM.read(3) == 0) && verificadescida!=1 && verificadescida!=3 && EEPROM.read(1)==0)) {        
-        
         while (digitalRead(SOBE) == HIGH);
-        analogWrite(LPWM_Output, 190); 
-        analogWrite(RPWM_Output, 0); 
-        posicaoFinal1 = EEPROM.read(0);
-        posicaoFinal2 = EEPROM.read(5);
+        func_subida();
+    }
 
-        int posFinalTotal = 0;
-        lcd.setBacklight(HIGH); 
-        lcd.clear(); 
-        lcd.setCursor(0, 0); 
-        lcd.print("Varal Subindo");
-        lcd.setCursor(0, 1); 
-        lcd.print(NOME); 
-        motor = 1;      
-
-        while(motor){
-            calcula();
-            //lcd.setCursor(10, 1);
-            //lcd.print(current);              
-            if(digitalRead(sensor)==HIGH){ 
-              while (digitalRead(sensor) == HIGH);
-              posFinalTotal++;
-              //lcd.setCursor(0, 1); 
-              //lcd.print(posFinalTotal);
-            }            
-            if(posFinalTotal == posicaoFinal1 + posicaoFinal2*255){
-              lcd.clear(); 
-              lcd.print("Chegou no topo"); 
-              analogWrite(LPWM_Output, 0); 
-              analogWrite(RPWM_Output, 0); 
-              motor = 0;}
-              verificadescida=3;
-            }
-        posicaoAtual1 = posicaoFinal1;
-        posicaoAtual2 = posicaoFinal2;
-        EEPROM.write(2, posicaoAtual1);
-        EEPROM.write(3, posicaoAtual2);
-        EEPROM.write(1, verificadescida);
- }
-//descida
+//Descida
     else if ((digitalRead(DESCE) == HIGH && estado==1 && verificadescida==3) || ((digitalRead(DESCE) == HIGH && estado==1 &&  (EEPROM.read(2)+EEPROM.read(3)*255) == (EEPROM.read(0)+EEPROM.read(5)*255)) && verificadescida!=1 && verificadescida!=3 && EEPROM.read(1)==0 )) {          
         while (digitalRead(DESCE) == HIGH);
-
-        lcd.setBacklight(HIGH); 
-        lcd.clear(); 
-        lcd.setCursor(0, 0); 
-        lcd.print("Varal Descendo");
-
-        lcd.setCursor(0, 1); 
-        lcd.print(NOME);
-        analogWrite(LPWM_Output, 0); 
-        analogWrite(RPWM_Output, 170); 
-        posicaoFinal1 = EEPROM.read(0);
-        posicaoFinal2 = EEPROM.read(5);
-        lcd.setCursor(0, 0); 
-        lcd.print("Varal Descendo");
-
-        lcd.setCursor(0, 1); 
-        lcd.print(NOME);
-        int posTotal = 255*posicaoFinal2 + posicaoFinal1; 
-        motor = 1;   
-
-        while(motor){     
-            calcula(); 
-            if(digitalRead(sensor)==HIGH){ 
-                while (digitalRead(sensor) == HIGH);
-                if(posTotal > 0){
-                    posTotal--;  
-                }
-            }
-            if(posTotal == 0){
-                lcd.clear(); 
-                lcd.print("Chegou embaixo"); 
-                analogWrite(LPWM_Output, 0); 
-                analogWrite(RPWM_Output, 0); 
-                motor = 0;
-            }
-        }
-        verificadescida=4;
-        posicaoAtual1 = 0;
-        posicaoAtual2 = 0;
-        EEPROM.write(2, posicaoAtual1);
-        EEPROM.write(3, posicaoAtual2);
-        EEPROM.write(1, verificadescida);
+        func_descida();
     }
               
 }
